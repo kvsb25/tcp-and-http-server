16d3373502ec45ec778dba5e6a9850d6f970dcc9
Kovidh <kovidhvsbhati@gmail.com>
2025-12-05 20:49:20 +0530

    define class TcpServer constructor

--- include/zen/tcp/tcpServer.hpp
+++ include/zen/tcp/tcpServer.hpp
@@ -5,10 +5,11 @@
 
 class TcpServer{
 private:
-    SOCKET main_socket;
+    SOCKET main_socket = INVALID_SOCKET;
     sockaddr_in server_addr;
+    WSADATA wsaData;
 public:
-    TcpServer(SOCKET& s, sockaddr_in& addr);
+    TcpServer(u_short port);
     ~TcpServer();
     void start();  // contains main loop and shoots a thread for client session handling
 };
---
+++ src/tcp/tcpServer.cpp
@@ -0,0 +1,27 @@
+#include "../../include/zen/tcp/clientSession.hpp"
+#include "../../include/zen/tcp/tcpServer.hpp"
+
+TcpServer::TcpServer(u_short port)
+{
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        // exitWithError
+    }
+
+    main_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if(main_socket < 0){
+        // exitWithError
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    // int bindStatus = bind(main_socket, (SOCKADDR *)&server_addr, sizeof(server_addr));
+    // if(bindStatus == SOCKET_ERROR){
+
+    // }
+
+    // int listenStatus = listen(main_socket, 20);
+}
