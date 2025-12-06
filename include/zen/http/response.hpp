72991412e075ebecfac413d14bd763f643b9a229
Kovidh <kovidhvsbhati@gmail.com>
2025-12-06 23:39:15 +0530

    define struct http::Response member functions

--- include/zen/http/response.hpp
+++ include/zen/http/response.hpp
@@ -2,6 +2,7 @@
 #include "utils.hpp"
 #include <vector>
 #include <sstream>
+#include <chrono>
 
 namespace http{
     struct Response{
@@ -12,7 +13,7 @@ namespace http{
 
         Response();
         Response& send(std::string& data);
-        Response& status(int status);
+        Response& status(int& code);
         Response& header(std::pair<std::string, std::string>& key_value_pair);
         Response& header(std::vector<std::pair<std::string, std::string>>& headers);
         Response& redirect(std::string& absolute_url);
--- src/http/response.cpp
+++ src/http/response.cpp
@@ -5,12 +5,42 @@ namespace http
     Response::Response()
     {
         status_code = 200;
-        status_message = message_for_status[status_code];
+        status_message = message_for_status.at(200);
+
+        auto iso = []{ auto t=time(nullptr); char b[32]; strftime(b,32,"%Y-%m-%dT%H:%M:%SZ", gmtime(&t)); return std::string(b); }();
+        headers["date"] = iso;
     }
 
     Response& Response::send(std::string &data)
     {
         body << data;
+        return *this;
+    }
+
+    Response& Response::status(int& code){
+        if(valid_codes.count(code)>0){
+            status_code = code;
+            status_message = message_for_status.at(code);
+        }
+        return *this;
+    }
+
+    Response& Response::header(std::pair<std::string, std::string>& pair){
+        headers.insert(pair);
+        return *this;
+    }
+
+    Response& Response::header(std::vector<std::pair<std::string, std::string>>& headers){
+        for(auto header : headers){
+            this->headers.insert(header);
+        }
+        return *this;
+    }
+
+    Response& Response::redirect(std::string& url){
+        std::pair<std::string, std::string> location_header = {"Location", url};
+        headers.insert(location_header);
+        return *this;
     }
 
 }
